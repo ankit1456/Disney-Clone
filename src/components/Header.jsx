6db8966e54091b1852e89d460c3d07a386b88838
Ankit Tripathi <ankittripathi1456@email.com>
2021-04-24 18:36:04 +0530

    login component done

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Navbar>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+      <NavMenu>Menu</NavMenu>
+    </Navbar>
+  );
+};
+
+const Navbar = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+`;
+export default Header;
